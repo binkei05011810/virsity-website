abecca970f3d2c2d835dc6a4b69febfe1022aece
binkei05011810 <minhkhue181001@gmail.com>
2021-06-21 12:29:00 +0300

    Add registration from Modal

---
+++ src/components/registration-form.js
@@ -0,0 +1,90 @@
+/** @jsx jsx */
+import { jsx, Box, Flex, Button } from "theme-ui";
+import {
+    Label,
+    Input,
+    Select,
+    Textarea,
+    Radio,
+    Checkbox,
+    Slider,
+} from 'theme-ui';
+
+const styles = ({
+    name: {
+        justifyContent: "space-between",
+    },
+
+    nameBox: {
+        width: "48%",
+    },
+
+    formContainer: {
+        width: "500px",
+        position: "relative",
+
+        label: {
+            fontSize: 1,
+            fontWeight: 'bold',
+        },
+        input: {
+            height: "40px",
+            borderColor: 'gray',
+            '&:focus': {
+                borderColor: 'primary',
+                boxShadow: t => `0 0 0 2px ${t.colors.primary}`,
+                outline: 'none',
+            },
+        },
+        textarea: {
+            borderColor: 'gray',
+            '&:focus': {
+                borderColor: 'primary',
+                boxShadow: t => `0 0 0 2px ${t.colors.primary}`,
+                outline: 'none',
+            },
+        },
+
+        "@media screen and (max-width:560px)": {
+            width: "90vw"
+        },
+    },
+
+    closeBtn: {
+        color: "black",
+        position: "absolute",
+        top: 0,
+        right: 0,
+        '&:hover': {
+            cursor: "pointer"
+        },
+    }
+})
+const RegistrationFrom = ({ closeModal }) => {
+    return (
+        <Box sx={styles.formContainer} as="form" onSubmit={(e) => e.preventDefault()}>
+            <Box sx={styles.closeBtn} onClick={closeModal}><i class="far fa-times-circle"></i></Box>
+            <Flex sx={styles.name}>
+                <Box sx={styles.nameBox}>
+                    <Label sx={styles.label} htmlFor="firstname">First name</Label>
+                    <Input sx={styles.input} name="firstname" id="firstname" mb={3} />
+                </Box>
+                <Box sx={styles.nameBox}>
+                    <Label sx={styles.label} htmlFor="lastname">Last name</Label>
+                    <Input sx={styles.input} name="firstname" id="lastname" mb={3} />
+                </Box>
+            </Flex>
+
+            <Flex mb={3}>
+                <Label><Radio name="letter" />Individual</Label>
+                <Label><Radio name="letter" />With team</Label>
+            </Flex>
+
+            <Label sx={styles.label} htmlFor="teamname">Team name (optional)</Label>
+            <Input sx={styles.input} name="teamname" id="teamname" mb={3} />
+            <Button>Submit</Button>
+        </Box>
+    )
+}
+
+export default RegistrationFrom;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,6 +15,8 @@ class CustomDocument extends Document {
             rel="stylesheet"
             crossOrigin="anonymous"
           />
+
+          <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
         </Head>
         <body>
           <Main />
